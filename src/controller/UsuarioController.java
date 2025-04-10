786c9f54bce77b0a324ed5ea221071b5c7a7a1ec
Higor-Carboni <168148469+Higor-Carboni@users.noreply.github.com>
2025-04-09 21:32:25 -0300

    Add files via upload

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -22,212 +22,239 @@ import utils.Utils;
  */
 public class UsuarioController {
 
-  public boolean autenticar(String email, String senha) {
-    //Montar o comando a ser executado
-    //os ? são variáveis que são preenchidas mais adiante
-    String sql = "SELECT * from TBUSUARIO "
-            + " WHERE email = ? and senha = ? "
-            + " and ativo = true";
+    public boolean autenticar(String email, String senha) {
+        //Montar o comando a ser executado
+        //os ? são variáveis que são preenchidas mais adiante
+        String sql = "SELECT * from TBUSUARIO "
+                + " WHERE email = ? and senha = ? "
+                + " and ativo = true";
 
-    //Cria uma instância do gerenciador de conexão(conexão com o banco de dados),
-    GerenciadorConexao gerenciador = new GerenciadorConexao();
-    //Declara as variáveis como nulas antes do try para poder usar no finally
-    PreparedStatement comando = null;
-    ResultSet resultado = null;
+        //Cria uma instância do gerenciador de conexão(conexão com o banco de dados),
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        //Declara as variáveis como nulas antes do try para poder usar no finally
+        PreparedStatement comando = null;
+        ResultSet resultado = null;
 
-    try {
-      //prepara o sql, analisando o formato e as váriaveis
-      comando = gerenciador.prepararComando(sql);
+        try {
+            //prepara o sql, analisando o formato e as váriaveis
+            comando = gerenciador.prepararComando(sql);
 
-      //define o valor de cada variável(?) pela posição em que aparece no sql
-      comando.setString(1, email);
-      comando.setString(2, senha);
+            //define o valor de cada variável(?) pela posição em que aparece no sql
+            comando.setString(1, email);
+            comando.setString(2, senha);
 
-      //executa o comando e guarda o resultado da consulta, o resultado é semelhante a uma grade
-      resultado = comando.executeQuery();
+            //executa o comando e guarda o resultado da consulta, o resultado é semelhante a uma grade
+            resultado = comando.executeQuery();
 
-      //resultado.next() - tenta avançar para a próxima linha, caso consiga retorna true
-      if (resultado.next()) {
-        //Se conseguiu avançar para a próxima linha é porque achou um usuário com aquele nome e senha
-        return true;
-      }
-    } catch (SQLException e) {//caso ocorra um erro relacionado ao banco de dados
-      JOptionPane.showMessageDialog(null, e.getMessage());//exibe popup com o erro
-    } finally {//depois de executar o try, dando erro ou não executa o finally
-      gerenciador.fecharConexao(comando, resultado);
-    }
-    return false;
-  }
-
-  public boolean inserirUsuario(Usuario usu) {
-    //Montar o comando a ser executado
-    //os ? são variáveis que são preenchidas mais adiante
-    String sql = "INSERT INTO TBUSUARIO(nome, email, senha, datanasc, ativo) "
-            + " VALUES (?,?,?,?,?)";
-
-    //Cria uma instância do gerenciador de conexão(conexão com o banco de dados),
-    GerenciadorConexao gerenciador = new GerenciadorConexao();
-    //Declara as variáveis como nulas antes do try para poder usar no finally
-    PreparedStatement comando = null;
-    try {
-      //prepara o sql, analisando o formato e as váriaveis
-      comando = gerenciador.prepararComando(sql);
-
-      //define o valor de cada variável(?) pela posição em que aparece no sql
-      comando.setString(1, usu.getNome());
-      comando.setString(2, usu.getEmail());
-      comando.setString(3, usu.getSenha());
-      comando.setDate(4, new java.sql.Date(usu.getDataNasc().getTime()));
-      comando.setBoolean(5, usu.isAtivo());
-
-      //Executa o insert
-      comando.executeUpdate();
-
-      return true;
-    } catch (SQLException e) {//caso ocorra um erro relacionado ao banco de dados
-      JOptionPane.showMessageDialog(null, e.getMessage());//exibe popup com o erro
-    } finally {//depois de executar o try, dando erro ou não executa o finally
-      gerenciador.fecharConexao(comando);
-    }
-    return false;
-  }
-
-  public boolean alterarUsuario(Usuario u) {
-    String sql = "UPDATE tbusuario SET nome = ?, "
-            + " email = ?";
-
-    if (u.getSenha() != null) {
-      sql = sql + " , senha = ? ";
+            //resultado.next() - tenta avançar para a próxima linha, caso consiga retorna true
+            if (resultado.next()) {
+                //Se conseguiu avançar para a próxima linha é porque achou um usuário com aquele nome e senha
+                return true;
+            }
+        } catch (SQLException e) {//caso ocorra um erro relacionado ao banco de dados
+            JOptionPane.showMessageDialog(null, e.getMessage());//exibe popup com o erro
+        } finally {//depois de executar o try, dando erro ou não executa o finally
+            gerenciador.fecharConexao(comando, resultado);
+        }
+        return false;
     }
 
-    sql = sql + " , datanasc = ?, ativo = ? WHERE pkusuario = ?";
+    public boolean inserirUsuario(Usuario usu) {
+        //Montar o comando a ser executado
+        //os ? são variáveis que são preenchidas mais adiante
+        String sql = "INSERT INTO TBUSUARIO(nome, email, senha, datanasc, ativo) "
+                + " VALUES (?,?,?,?,?)";
 
-    GerenciadorConexao gerenciador = new GerenciadorConexao();
-    PreparedStatement comando = null;
+        //Cria uma instância do gerenciador de conexão(conexão com o banco de dados),
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        //Declara as variáveis como nulas antes do try para poder usar no finally
+        PreparedStatement comando = null;
+        try {
+            //prepara o sql, analisando o formato e as váriaveis
+            comando = gerenciador.prepararComando(sql);
 
-    try {
-      comando = gerenciador.prepararComando(sql);
+            //define o valor de cada variável(?) pela posição em que aparece no sql
+            comando.setString(1, usu.getNome());
+            comando.setString(2, usu.getEmail());
+            comando.setString(3, usu.getSenha());
+            comando.setDate(4, new java.sql.Date(usu.getDataNasc().getTime()));
+            comando.setBoolean(5, usu.isAtivo());
 
-      comando.setString(1, u.getNome());
-      comando.setString(2, u.getEmail());
+            //Executa o insert
+            comando.executeUpdate();
 
-      int numCampo = 3;
-      
-      if (u.getSenha() != null) {
-        comando.setString(numCampo, u.getSenha()); //numCampo = 3
-        numCampo++;
-      }
-      
-      //numCampo = 3 ou 4
-      comando.setDate(numCampo, new java.sql.Date(u.getDataNasc().getTime()));
-      numCampo++;
-      //numCampo = 4 ou 5
-      comando.setBoolean(numCampo, u.isAtivo());
-      numCampo++;
-      //numCampo = 5 ou 6
-      comando.setInt(numCampo, u.getPkUsuario());
-
-      comando.executeUpdate();
-
-      return true;
-    } catch (SQLException ex) {
-      JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + ex);
-    } finally {
-      gerenciador.fecharConexao(comando);
+            return true;
+        } catch (SQLException e) {//caso ocorra um erro relacionado ao banco de dados
+            JOptionPane.showMessageDialog(null, e.getMessage());//exibe popup com o erro
+        } finally {//depois de executar o try, dando erro ou não executa o finally
+            gerenciador.fecharConexao(comando);
+        }
+        return false;
     }
-    return false;
-  }
 
-  public Usuario buscarPorPk(int pkUsuario) {
-    //Guarda o sql
-    String sql = "SELECT * FROM tbusuario "
-            + " WHERE PKUSUARIO = ? ";
+    public boolean alterarUsuario(Usuario u) {
+        String sql = "UPDATE tbusuario SET nome = ?, "
+                + " email = ?";
 
-    //Cria um gerenciador de conexão
-    GerenciadorConexao gerenciador = new GerenciadorConexao();
-    //Cria as variáveis vazias antes do try pois vão ser usadas no finally
-    PreparedStatement comando = null;
-    ResultSet resultado = null;
+        if (u.getSenha() != null) {
+            sql = sql + " , senha = ? ";
+        }
 
-    //Crio um usuário vazio
-    Usuario usu = new Usuario();
+        sql = sql + " , datanasc = ?, ativo = ? WHERE pkusuario = ?";
 
-    try {
-      //Preparo do comando sql
-      comando = gerenciador.prepararComando(sql);
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        PreparedStatement comando = null;
 
-      comando.setInt(1, pkUsuario);
+        try {
+            comando = gerenciador.prepararComando(sql);
 
-      //Executo o comando e guardo o resultado
-      resultado = comando.executeQuery();
+            comando.setString(1, u.getNome());
+            comando.setString(2, u.getEmail());
 
-      //Irá percorrer os registros do resultado do sql
-      //A cada next() a variavel resultado aponta para o próximo registro 
-      //enquanto next() == true quer dizer que tem registros
-      if (resultado.next()) {
+            int numCampo = 3;
 
-        //Leio as informações da variável resultado e guardo no usuário
-        usu.setPkUsuario(resultado.getInt("pkusuario"));
-        usu.setNome(resultado.getString("nome"));
-        usu.setEmail(resultado.getString("email"));
-        usu.setSenha(resultado.getString("senha"));
-        usu.setDataNasc(resultado.getDate("datanasc"));
-        usu.setAtivo(resultado.getBoolean("ativo"));
-      }
+            if (u.getSenha() != null) {
+                comando.setString(numCampo, u.getSenha()); //numCampo = 3
+                numCampo++;
+            }
 
-    } catch (SQLException ex) {
-      Logger.getLogger(UsuarioController.class.getName()).log(
-              Level.SEVERE, null, ex);
-    } finally {
-      gerenciador.fecharConexao(comando, resultado);
+            //numCampo = 3 ou 4
+            comando.setDate(numCampo, new java.sql.Date(u.getDataNasc().getTime()));
+            numCampo++;
+            //numCampo = 4 ou 5
+            comando.setBoolean(numCampo, u.isAtivo());
+            numCampo++;
+            //numCampo = 5 ou 6
+            comando.setInt(numCampo, u.getPkUsuario());
+
+            comando.executeUpdate();
+
+            return true;
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + ex);
+        } finally {
+            gerenciador.fecharConexao(comando);
+        }
+        return false;
     }
-    //retorno o usuário
-    return usu;
-  }
 
-  public List<Usuario> consultar() {
-      String sql = "SELECT * FROM tbusuario";
-      
-      GerenciadorConexao gerencidor = new GerenciadorConexao();
-      //cria as variaveis vazias antes do try pois vão ser usadas no finally
-      PreparedStatement comando = null;
-      ResultSet resultado = null;
-      //crio a lista de usuairo vazia
-      List<Usuario> listaUsuarios = new ArrayList<>();
-      
-      try {
-          //Preparo do comando sql
-          comando = gerencidor.prepararComando(sql);
-          //Como não há parâmetros já executo direto
-          resultado = comando.executeQuery();
-          
-          //irá percorrer ps registros do resultado do sql
-          //a cada next(0 a variavel resultado aponta para o proximo registro
-          //enquanto next() == true quer dizer que tem registros
-          while (resultado.next()) {
-              //crio um novo usuario vazio
-              Usuario usuario = new Usuario();
-              
-              //leio as informações da variavel resultado e guardo no usurio
-              usuario.setPkUsuario(resultado.getInt("pkusuario"));
-              usuario.setNome(resultado.getString("nome"));
-              usuario.setEmail(resultado.getString("email"));
-              usuario.setSenha(resultado.getString("senha"));
-              usuario.setDataNasc(resultado.getDate("datanasc"));
-              usuario.setAtivo(resultado.getBoolean("ativo"));
-              
-              //adiciono o usuario na lista
-              listaUsuarios.add(usuario);
-          } 
-      } catch (SQLException ex) {
-          Logger.getLogger(UsuarioController.class.getName()).log(
+    public Usuario buscarPorPk(int pkUsuario) {
+        //Guarda o sql
+        String sql = "SELECT * FROM tbusuario "
+                + " WHERE PKUSUARIO = ? ";
+
+        //Cria um gerenciador de conexão
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        //Cria as variáveis vazias antes do try pois vão ser usadas no finally
+        PreparedStatement comando = null;
+        ResultSet resultado = null;
+
+        //Crio um usuário vazio
+        Usuario usu = new Usuario();
+
+        try {
+            //Preparo do comando sql
+            comando = gerenciador.prepararComando(sql);
+
+            comando.setInt(1, pkUsuario);
+
+            //Executo o comando e guardo o resultado
+            resultado = comando.executeQuery();
+
+            //Irá percorrer os registros do resultado do sql
+            //A cada next() a variavel resultado aponta para o próximo registro 
+            //enquanto next() == true quer dizer que tem registros
+            if (resultado.next()) {
+
+                //Leio as informações da variável resultado e guardo no usuário
+                usu.setPkUsuario(resultado.getInt("pkusuario"));
+                usu.setNome(resultado.getString("nome"));
+                usu.setEmail(resultado.getString("email"));
+                usu.setSenha(resultado.getString("senha"));
+                usu.setDataNasc(resultado.getDate("datanasc"));
+                usu.setAtivo(resultado.getBoolean("ativo"));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioController.class.getName()).log(
                     Level.SEVERE, null, ex);
-      } finally {
-          gerencidor.fecharConexao(comando, resultado);
-      }
-      //retorno a lista de usuarios
-      return listaUsuarios;
-  }
-  
+        } finally {
+            gerenciador.fecharConexao(comando, resultado);
+        }
+        //retorno o usuário
+        return usu;
+    }
 
+    public List<Usuario> consultar() {
+        //Guarda o sql
+        String sql = "SELECT * FROM tbusuario";
+
+        //Cria um gerenciador de conexão
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        //Cria as variáveis vazias antes do try pois vão ser usadas no finally
+        PreparedStatement comando = null;
+        ResultSet resultado = null;
+
+        //Crio a lista de usuários vazia
+        List<Usuario> listaUsuarios = new ArrayList<>();
+
+        try {
+            //Preparo do comando sql
+            comando = gerenciador.prepararComando(sql);
+
+            //Como não há parâmetros já executo direto
+            resultado = comando.executeQuery();
+
+            //Irá percorrer os registros do resultado do sql
+            //A cada next() a variavel resultado aponta para o próximo registro 
+            //enquanto next() == true quer dizer que tem registros
+            while (resultado.next()) {
+
+                //Crio um novo usuário vazio
+                Usuario usuario = new Usuario();
+
+                //Leio as informações da variável resultado e guardo no usuário
+                usuario.setPkUsuario(resultado.getInt("pkusuario"));
+                usuario.setNome(resultado.getString("nome"));
+                usuario.setEmail(resultado.getString("email"));
+                usuario.setSenha(resultado.getString("senha"));
+                usuario.setDataNasc(resultado.getDate("datanasc"));
+                usuario.setAtivo(resultado.getBoolean("ativo"));
+
+                //adiciono o usuário na lista
+                listaUsuarios.add(usuario);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioController.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        } finally {
+            gerenciador.fecharConexao(comando, resultado);
+        }
+
+        //retorno a lista de usuários
+        return listaUsuarios;
+    }
+
+    public boolean deletar(int pkUsuario) {
+        String sql = "DELETE FROM tbusuario WHERE pkUsuario = ?";
+
+        GerenciadorConexao gerenciador = new GerenciadorConexao();
+        PreparedStatement comando = null;
+
+        try {
+            comando = gerenciador.prepararComando(sql);
+
+            comando.setInt(1, pkUsuario);
+            comando.executeUpdate();
+            
+            return true;
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            gerenciador.fecharConexao(comando);
+        }
+        return false;
+    }
 }
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -6,81 +6,79 @@ import java.util.Date;
  * @author iband
  */
 public class Usuario {
+  private int pkUsuario;
+  private String nome;
+  private String email;
+  private String senha;
+  private Date dataNasc;
+  private boolean ativo;
 
-    private int pkUsuario;
-    private String nome;
-    private String email;
-    private String senha;
-    private Date dataNasc;
-    private boolean ativo;
-
-    public Usuario() {
-    }
-
-    ;
+  public Usuario(){};
   
   public Usuario(int pkUsuario, String nome, String email, String senha, Date dataNasc, boolean ativo) {
-        this.pkUsuario = pkUsuario;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.dataNasc = dataNasc;
-        this.ativo = ativo;
-    }
+    this.pkUsuario = pkUsuario;
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+    this.dataNasc = dataNasc;
+    this.ativo = ativo;
+  }
 
-    public int getPkUsuario() {
-        return pkUsuario;
-    }
+  public int getPkUsuario() {
+    return pkUsuario;
+  }
 
-    public void setPkUsuario(int pkUsuario) {
-        this.pkUsuario = pkUsuario;
-    }
+  public void setPkUsuario(int pkUsuario) {
+    this.pkUsuario = pkUsuario;
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getSenha() {
-        return senha;
-    }
+  public String getSenha() {
+    return senha;
+  }
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
 
-    public Date getDataNasc() {
-        return dataNasc;
-    }
+  public Date getDataNasc() {
+    return dataNasc;
+  }
 
-    public void setDataNasc(Date dataNasc) {
-        this.dataNasc = dataNasc;
-    }
+  public void setDataNasc(Date dataNasc) {
+    this.dataNasc = dataNasc;
+  }
 
-    public boolean isAtivo() {
-        return ativo;
-    }
+  public boolean isAtivo() {
+    return ativo;
+  }
 
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
+  public void setAtivo(boolean ativo) {
+    this.ativo = ativo;
+  }
 
-    public String ativoToString() {
-        if (ativo == true) {
-            return "ativo";
-        } else {
-            return "inativo";
-        }
+  public String ativoToString() {
+    if(ativo){
+      return "Ativo";
+    } else {
+      return "Inativo";
     }
+  }
+  
+  
 }
